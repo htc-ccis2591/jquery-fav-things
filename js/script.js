97adf61dfb856de545f4259f5bfe767270b09b08
Ryan Freemark <freemark21@gmail.com>
2016-02-15 11:45:54 -0600

    Basic function and style

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $(function() {
     $('h2').hide();
     $('p').hide();
+    $('img').hide();
+    
+    $('#begin').on('click', function(){
+        $('header').fadeOut(500);
+        $('img').show();
+      });
     
     $('.favorite').on('click', function(){
-        $(this).siblings().toggle();
+        $(this).siblings().slideToggle();
     });
 
     
