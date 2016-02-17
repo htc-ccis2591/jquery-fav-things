afd553cfc46a6727a28d4d7e31a978aa31ead83a
dechmann <tdechmann001@my.hennepintech.edu>
2016-02-17 16:28:52 -0600

    new changes

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function () {
+    $("#content").hide(); //hides item
+    
+    $("h1").on("click", function() {
+        $("#content").show();
+        $("p").hide(); 
+        $("img").hide();
+        $("h1").remove(); //gets ride of the h1
+        $("#content").prepend("Click on one of the title's of my favorite things."); //tells user how it works
+        });
+    //This peice is to display the content and image, it also hides the other content and images.
+    $("h2").on("click", function() {
+        $("p").hide();
+        $("img").hide();
+        $(this).next().next().show();
+        $(this).next().show();
+        $("h1").remove();
+    });
+});
