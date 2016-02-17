3f99c11f37b24a0bd72a4217b0e933e61de538cf
dechmann <tdechmann001@my.hennepintech.edu>
2016-02-17 17:55:46 -0600

    new chagnes

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ $(function () {
         $("p").hide(); 
         $("img").hide();
         $("h1").remove(); //gets ride of the h1
-        $("#content").prepend("Click on one of the title's of my favorite things."); //tells user how it works
+        $("#content").before("Click on one of the title's of my favorite things."); //tells user how it works
+        //$("h1").replaceWith("Click on one of the title's of my favorite things."); //works like like 8 and 9.
         });
     //This peice is to display the content and image, it also hides the other content and images.
     $("h2").on("click", function() {
