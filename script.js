d3909b1f122df7c305312af9232e10122e9a8a63
chee-yang <cyang034@my.hennepintech.edu>
2016-10-08 16:41:17 -0500

    Update

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
 $(function () {
 
-    var $divs = $("div");
-    //var $game = $("#game");
-    //var $movie = $("#movie");
-    //var $food = $("#food");
-    var $game = $divs.get(0);
-    var $movie = $divs.get(1);
-    var $food = $divs.get(2);
+    //--Creates page, adding content.
+    var createPage = function () {
+        $("h1").append("<h2>Click to Reveal a List of Secrets </h2>");
+        $("#intro").append("<h3>Click on each heading to display information.</h3>");
+        $("#intro").append("<div id='game'><h2 id='gameh2'>VideoGame</h2></div>");
+        $("#intro").append("<div id='movie'><h2 id='movieh2'>Movie</h2></div>");
+        $("#intro").append("<div id='food'><h2 id='foodh2'>Food</h2></div>");
 
-    console.log($divs);
-    console.log($divs[0]);
-    console.log($divs.get(0));
+        $("#game").append("<p id='gamep' class = 'hide' > I enjoy playing Role Playing Games(RPG) such as the Final Fantasy series, First Person Shooting / Shooter(FPS) games such as Call of Duty(COD) and Black Ops(BO), arcade games such as Metal Slug.</p>");
 
-    $("h1").html("Click to Reveal a List of Secrets");
-    $divs.addClass("hide");
-    $($game).attr("id", "game");
-    $($movie).attr("id", "movie");
-    $($food).attr("id", "food");
+        $("#movie").append("<p id='moviep' class = 'hide' > I enjoy playing Role Playing Games(RPG) such as the Final Fantasy series, First Person Shooting / Shooter(FPS) games such as Call of Duty(COD) and Black Ops(BO), arcade games such as Metal Slug.</p>");
 
-    $("h1").on("click", function () {
-        $divs.removeClass("hide");
-        $("h1").after("<p>Click on each heading to display information.</p>")
-        $($game).append("<h2>Video Games</h2>");
-        $($movie).append("<h2>Movies</h2>");
-        $($food).append("<h2>Food</h2>")
+        $("#food").append("<p id='foodp' class = 'hide' > I enjoy playing Role Playing Games(RPG) such as the Final Fantasy series, First Person Shooting / Shooter(FPS) games such as Call of Duty(COD) and Black Ops(BO), arcade games such as Metal Slug.</p>");
+    };
 
-    })
+    createPage();
 
-    $($game).on("click", function () {
-        $($game).append("<p>I enjoy playing Role Playing Games(RPG) such as the Final Fantasy series, First Person Shooting/Shooter(FPS) games such as Call of Duty(COD) and Black Ops(BO), arcade games such as Metal Slug.</p>")
-    })
+    //--OnClick Events
+    $("h1").on("click", (function () {
+        $("#intro").toggle();
+    }));
 
-    $($movie).on("click", function () {
-        $($movie).append("<p>I enjoyy watching movies of all genres, action, horror, comedies, documentaries, drama, sci-fi, thrillers, and anime.</p>")
-    })
+    $("#gameh2").on("click", (function () {
+        $("#gamep").toggle();
+    }));
 
+    $("#movie").on("click", (function () {
+        $("#moviep").toggle();
+    }));
 
-    $($food).on("click", function () {
-        $($food).append("<p>I love, LOVE food! I mean, who doesn't!? I enjoy food off all nationalities from Asian, Mexican, Thai, Greek, Italian, and American food.")
-    })
+    $("#food").on("click", (function () {
+        $("#foodp").toggle();
+    }));
 });
